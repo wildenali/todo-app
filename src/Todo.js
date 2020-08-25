2389b37833960a7ecbd221d89bf0251936e22196
wildenali <wildenwildenaliali@gmail.com>
2020-08-25 23:28:53 +0700

    nambahit button edit dan modal pop up ketika edit button di pencet

--- src/Todo.js
+++ src/Todo.js
@@ -1,19 +1,42 @@
-import React from 'react'
-import { List, ListItem, ListItemText, ListItemAvatar, Button } from '@material-ui/core'
+import React, { useState } from 'react'
+import { List, ListItem, ListItemText, ListItemAvatar, Button, Modal } from '@material-ui/core'
 import db from './firebase';
 import './Todo.css'
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever'
 
 function Todo(props) {
+
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true)
+  };
+  
+  const handleClose = () => {
+    setOpen(false)
+  };
+ 
   return (
-    <List className="todo_list">
-      <ListItem>
-        <ListItemAvatar>
-        </ListItemAvatar>
-        <ListItemText primary={props.todo.todo} secondary="Dummy deadline ♣" />
-      </ListItem>
-      <DeleteForeverIcon onClick={event => db.collection('todos').doc(props.todo.id).delete()} />
-    </List>
+    <>
+      <Modal
+        open={open}
+        onClose={e => setOpen(false)}
+      >
+        <div>
+          <h1>I am a Modal</h1>
+          <button onClick={e => setOpen(false)}></button>
+        </div>
+      </Modal>
+      <List className="todo_list">
+        <ListItem>
+          <ListItemAvatar>
+          </ListItemAvatar>
+          <ListItemText primary={props.todo.todo} secondary="Dummy deadline ♣" />
+        </ListItem>
+        <button onClick={e => setOpen(true)}>Edit</button>
+        <DeleteForeverIcon onClick={event => db.collection('todos').doc(props.todo.id).delete()} />
+      </List>
+    </>
   )
 }
 
